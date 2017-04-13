3f0127228dec59f2b0644e6ac5a11386e5b8bc83
fangqin <fangqin@mainiway.com>
2017-04-13 12:57:02 +0800

    updata

---
+++ github_use.js
@@ -0,0 +1,14 @@
+/*
+下载最新git客户端地址
+https://git-for-windows.github.io/
+克隆github账号里的项目到本地
+git clone 程序地址
+将本地的改动添加到本地版本管理器
+git add .
+提交本地代码到本地版本控制库
+git commit -m 描述信息
+拉取远端版本控制库合并本地控制库
+git pull
+提交本地版本控制库的代码到github账号
+git push -u origin master
+*/
